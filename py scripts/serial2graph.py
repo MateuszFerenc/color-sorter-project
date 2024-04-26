4005d316b70c80fc3747eafdb373d78d8363ddd1
MateuszFerenc <mateoferenc@gmail.com>
2024-04-26 23:01:13 +0200

    More updates for serial2graph.py file Further work needed TODO: - add logic to draw selected parameters onto graph

--- py scripts/serial2graph.py
+++ py scripts/serial2graph.py
@@ -2,6 +2,8 @@ import serial
 import serial.tools.list_ports
 from atexit import register as register_exit
 import matplotlib.pyplot as plotter
+from matplotlib import colors as plotter_colors
+from re import search
 
 serial_handler = None
 
@@ -94,6 +96,100 @@ plotter.grid(visible=True, linestyle=':', linewidth=0.5)
 data = []
 
 print(f"Serial is open: {serial_handler.is_open}")
+
+samples_amount = 0
+
+try:
+	samples_amount = int(input("Choose amount of samples to display to choose from\n>"))
+	assert samples_amount > 0
+	assert samples_amount <= 10
+except ValueError:
+	print(f"{samples_amount} is not a valid value.")
+	input()
+	exit(3)
+except AssertionError:
+	print(f"{samples_amount} should be in range (0, 10>.")
+	input()
+	exit(3)
+
+samples = []
+
+serial_handler.timeout = None
+
+for i in range(samples_amount):
+	line_raw = str(serial_handler.readline().decode('utf-8')).replace('\n', r'\n').replace('\t', '').replace('\r', '').replace("\x1b[2J", '').replace("\x1b[H", '')
+	separator = search("[:\-=]", line_raw)
+	line = None
+	if separator:
+		line = line_raw.split(line_raw[separator.start()])
+		separator = line_raw[separator.start()]
+	else:
+		separator = None
+		line = line_raw
+	samples.append(
+		{
+			"sample_no": i,
+			"separator": separator,
+			"line_raw": line_raw,
+			"parameter": line[0].strip() if len(line) > 1 else "Not found",
+			"value": line[1].replace(r'\n', '').strip() if len(line) > 1 else "Not found"
+		}
+	)
+	print(f"Sample: \t{i}\n"
+	   		f"Separator: \t{samples[-1]['separator']}\n"
+			f"Raw line: \t{samples[-1]['line_raw']}\n"
+			f"Possible parameter: \t{samples[-1]['parameter']}\n"
+			f"Possible value: \t{samples[-1]['value']}\n")
+
+parameters = []
+
+for sample in samples:
+	parameter = sample['parameter']
+	if parameter != "Not found":
+		if parameter not in (sublist['parameter'] for sublist in parameters):
+			parameters.append(
+				{
+					"parameter": sample['parameter'],
+					"position": sample['sample_no'],
+					"configured": False
+				}
+			)
+
+print(f"Found following parameters: {', '.join(sublist['parameter'] for sublist in parameters)}\n")
+
+configured = [sublist['configured'] for sublist in parameters]
+
+while not all(configured):
+	selection = str(input("Select parameter to configure (type \"\\exit\" to exit configure mode)\n>"))
+	if selection == "\exit":
+		print("Following parameters are not configured:")
+		for no, is_configured in enumerate(configured):
+			if not is_configured:
+				print(f"{parameters[no]['parameter']}", end=', ' if no < len(configured) - 1 else '\n')
+		choice = input("Are you sure want to exit? (N/y)>")
+		if choice.lower() in ("yes", "y", "yep", "yeah"):
+			break
+	elif selection in tuple(sublist['parameter'] for sublist in parameters):
+		label = input("Enter parameter label>")
+		color = input("Enter color>")
+		if color not in plotter_colors.cnames.keys() and not search("^#[0-9A-Fa-f]{6}$", color):
+			print(f"{color} is not correct.")
+			continue
+		types = ("int", "uint", "hex", "bin")
+		type = input(f"Parameter datatype ({', '.join(types)})>")
+		if type not in types:
+			print(f"{type} is not correct.")
+			continue
+		position = tuple(sublist['parameter'] for sublist in parameters).index(selection)
+		configured[position] = True
+		parameters[position]['configured'] = True
+		
+	else:
+		print(f"{selection} is not a know parameter.\nTry again.")
+		
+input()
+
+serial_handler.timeout = 0
 
 while True:
 	line = str(serial_handler.readline())
