c8865c18097ebb63c628e6cd4c456f2998d4cd80
MateuszFerenc <mateoferenc@gmail.com>
2024-05-24 18:08:53 +0200

    Keypad and SoftPWM working SoftPWm could be enchanced

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/cpufunc.h>
+#include <avr/pgmspace.h>
+
+#define PIN_clear(port, pin)                port &= ~( 1 << pin )
+#define PIN_set(port, pin)                  port |= ( 1 << pin )
+#define PIN_toggle(port, pin)               port ^= ( 1 << pin )
+#define PIN_control(port, pin, state)       port = ( port & ~( 1 << pin ) ) | ( state << pin )
+#define is_true(value)                      value ? 1 : 0
 
 // sense stages ADC channels PORTA
 #define metal_sense_adc 0
@@ -77,12 +84,11 @@
 Buzzer                  (PD2)
 */
 
-char selected_adc_channel = metal_sense_adc;
 
 uint16_t metal_sense_buffer[10] = { 0 }, glass_sense_buffer[10] = { 0 }, color_sense_buffer[10] = { 0 }, metal_sense_value = 0, glass_sense_value = 0, color_sense_value = 0;
-uint8_t adc_read_count = 0;
 uint16_t system_counter = 0;
-uint8_t sec, min, hour, adc_hold = 0;
+uint8_t sec = 0, min = 0, hour = 0;
+uint8_t adc_hold = 0, adc_read_count = 0;
 uint8_t lcdColumns, lcdRows, currentCol, currentRow, lcdRowStart[4];
 
 void USART_Transmit( unsigned char data );
@@ -98,9 +104,9 @@ void lcd_write_nibble(uint8_t data);
 void lcd_move_cursor(uint8_t row, uint8_t col);
 void lcd_init(void);
 
-
+// System interrupt (every 0.625us)
 ISR(TIMER2_COMP_vect){
-    PORTB ^= ( 1 << PB2 );
+    static uint8_t selected_adc_channel = metal_sense_adc;
     if ( ADCSRA & ( 1 << ADIF ) ) {         // ADC conversion complete flag
         if ( adc_hold ) {
             if ( adc_hold > 3 ) {       // wait with starting new conversion for ca. 6 ms after changing mux
@@ -133,32 +139,14 @@ ISR(TIMER2_COMP_vect){
             adc_hold = 1;       // hold conversion for stable readings
         }
     }
-    if ( system_counter > 200 ){
-        USART_text("Interrupt! (200 * 0.625ms = 125ms)\n\r");
-        //unsigned char message[15];
-
-        //USART_text("\e[2J");
-        //USART_text("\e[H");
-
-        //sprintf(message, "R = 0x%x\n\r", metal_sense_value / 10);
-        //USART_text(message);        
-
-        // sprintf(message, "G = 0x%x\n\r", GREEN_value / 10);
-        // USART_text(message);
-
-        // sprintf(message, "B = 0x%x\n\r", BLUE_value / 10);
-        // USART_text(message);
-
-        // unsigned char time[24];
-
-        // sprintf(time, "\nRuntime: %d:%d:%d", hour, min, sec);
-        // USART_text(time);
+    if ( system_counter % 256 == 0 && system_counter != 0 ){
+        USART_text("Interrupt! (256 * 0.625ms = 160ms)\n\r");
     }
     // correction available
     // 2^16 = 65536 when 2^16 / 1600 = 40.96, which means that every counter reload lacks 64 ticks
     // so when we correct modulo after missing cycle, we can achieve stable clock
     // like one run -> mod 1600 => reload counter (missing 64 cycles) -> first mod 64 then back to mod 1600, and repeat
-    if ( system_counter % 1600 == 0 ){
+    if ( system_counter % 1600 == 0 && system_counter != 0 ){
         sec++;
         if ( ( sec & 0x0F ) > 9 ){
             sec += 0x10;
@@ -187,32 +175,75 @@ ISR(TIMER2_COMP_vect){
     system_counter++;
 }
 
-uint8_t key_col_state[4] = { 0 };
+uint8_t compare_PWM0, compare_PWM1, compare_PWM2;
+volatile uint8_t compbuff_PWM0, compbuff_PWM1, compbuff_PWM2;
+uint8_t actual_num_key = '-', last_num_key = '-', actual_func_key = '-', last_func_key = '-';
 
+#define PWM0_pin        PB3
+#define PWM0_port       PORTB
+#define PWM1_pin        PD5
+#define PWM1_port       PORTD
+#define PWM2_pin        PD7
+#define PWM2_port       PORTD
+
+#define PWM0            0
+#define PWM1            1
+#define PWM2            2
+
+const unsigned char keypad_num0_keys[] PROGMEM = "-12-3";
+const unsigned char keypad_num1_keys[] PROGMEM = "-45-6";
+const unsigned char keypad_num2_keys[] PROGMEM = "-78-9";
+const unsigned char keypad_num3_keys[] PROGMEM = "-*0-#";
+const unsigned char keypad_func_keys[] PROGMEM = "-ABCD";
+
+// SoftPWM and Keypad scan interrupt (every 78.125us)
 ISR(TIMER0_COMP_vect){
     static uint8_t pwm_counter = 0xFF;
     static uint8_t key_scan_row = 0;
-    //static uint8_t key_col_state[4] = { 0 };
+    static uint8_t key_col_state[4] = { 0 };
+    static uint8_t pwm_mask = 0;
+    
+    PIN_control(PWM0_port, PWM0_pin, (pwm_mask & 1));
+    PIN_control(PWM1_port, PWM1_pin, (pwm_mask & 2) >> 1);
+    PIN_control(PWM2_port, PWM2_pin, (pwm_mask & 4) >> 2);
 
     if ( ++pwm_counter == 0 ){
-        PORTB ^= ( 1 << PB0 );
+        compare_PWM0 = compbuff_PWM0;
+        compare_PWM1 = compbuff_PWM1;
+        compare_PWM2 = compbuff_PWM2;
+
+        pwm_mask = ( 1 << PWM2 ) | ( 1 << PWM1 ) | ( 1 << PWM0 );
     }
+
+    if ( pwm_counter == compare_PWM0)   pwm_mask &= ~( 1 << PWM0 );
+    if ( pwm_counter == compare_PWM1)   pwm_mask &= ~( 1 << PWM1 );
+    if ( pwm_counter == compare_PWM2)   pwm_mask &= ~( 1 << PWM2 );
 
     if ( pwm_counter % 51 == 0 && pwm_counter != 0){       // scan next row every 1.25ms
         key_col_state[key_scan_row] = ~(PINB >> 4) & 0x0F;
         PORTC |= 0xF0;
         PORTC ^= (1 << (4 + key_scan_row++));
-        if ( key_scan_row > 3 )
+        if ( key_scan_row > 3 ) {
             key_scan_row = 0;
+            last_func_key = actual_func_key;
+            last_num_key = actual_num_key;
+            actual_func_key = pgm_read_byte(&keypad_func_keys[ ( key_col_state[1] >> 3 ) | ( key_col_state[2] >> 3 ) * 2 | ( key_col_state[3] >> 3 ) * 3 | ( key_col_state[0] >> 3 ) * 4 ]);
+            //actual_num_key = pgm_read_byte(&keypad_num_keys[ ( key_col_state[1] & 7 ) | ( key_col_state[2] & 7 ) * 2 | ( key_col_state[3] & 7 ) * 3 | ( key_col_state[0] & 7 ) * 4 ]);
+            if ( (key_col_state[1] & 7) && last_num_key == '-' )
+                actual_num_key = pgm_read_byte(&keypad_num0_keys[key_col_state[1] & 7 ]);
+            if ( (key_col_state[2] & 7) && last_num_key == '-' )
+                actual_num_key = pgm_read_byte(&keypad_num1_keys[key_col_state[2] & 7 ]);
+            if ( (key_col_state[3] & 7) && last_num_key == '-' )
+                actual_num_key = pgm_read_byte(&keypad_num2_keys[key_col_state[3] & 7 ]);
+            if ( (key_col_state[0] & 7) && last_num_key == '-' )
+                actual_num_key = pgm_read_byte(&keypad_num3_keys[key_col_state[0] & 7 ]);
+            if ( ( (key_col_state[0] & 7) + (key_col_state[1] & 7) + (key_col_state[2] & 7) + (key_col_state[3] & 7) ) == 0 )
+                actual_num_key = '-';
+        }
     }
-
-    //PORTB ^= ( 1 << PB0 );
-    //if ( ++pwm_comp == 0)
-    //    PORTB ^= ( 1 << PB1 );
 }
 
 ISR(BADISR_vect){}
-
 
 
 void lcd_text(unsigned char* text){
@@ -232,13 +263,13 @@ void lcd_text(unsigned char* text){
 }
 
 void lcd_data(uint8_t data){
-    PORTD |= ( 1 << PD3 );
+    PIN_set(PORTD, PD3);
     lcd_write_nibble(data >> 4);
     lcd_write_nibble(data);
 }
 
 void lcd_command(uint8_t command){
-    PORTD &= ~( 1 << PD3 );
+    PIN_clear(PORTD, PD3);
     lcd_write_nibble(command >> 4);
     lcd_write_nibble(command);
 }
@@ -248,9 +279,9 @@ void lcd_write_nibble(uint8_t data){
     PORTC |= (data & 0x0F);
     wait_us(250);
 
-    PORTD &= ~( 1 << PD6 );
-    PORTD |= ( 1 << PD6 );
-    PORTD &= ~( 1 << PD6 );
+    PIN_clear(PORTD, PD6);
+    PIN_set(PORTD, PD6);
+    PIN_clear(PORTD, PD6);
 
     wait_ms(2);
 }
@@ -263,12 +294,14 @@ void lcd_move_cursor(uint8_t row, uint8_t col){
 
 void lcd_clear(void){
     lcd_command(0x01);
+    wait_ms(2);
 }
 
 void lcd_home(void){
     currentCol = 0;
     currentRow = 0;
     lcd_command(0x02);
+    wait_ms(2);
 }
 
 void lcd_init(void){
@@ -294,6 +327,8 @@ void lcd_init(void){
     
     lcd_home();
 }
+
+//unsigned char format_dec(un)
 
 void USART_Init( unsigned int ubrr){
     UBRRH = (unsigned char)(ubrr>>8);
@@ -346,13 +381,13 @@ void setup(void){
     PORTC = 0xF0;
     PORTD = 0x04;
 
-    ADMUX = ( 1 << REFS0) | (selected_adc_channel & 0x1F);
+    ADMUX = ( 1 << REFS0) | (metal_sense_adc & 0x1F);
     ADCSRA = ( 1 << ADEN ) | ( 1 << ADSC ) | (1 << ADPS2 ) | ( 1 << ADPS1 ) | ( 1 << ADPS0 );   // Adc single shot mode, clk/128
 
     // Software PWM timer (50Hz)
-    // 50 Hz with 8bit resolution => update every 20ms times 256 => update every 0.078125 ms
-    TCCR0 = ( 1 << WGM01 ) | ( 1 << CS01 );                     // Timer0 in CTC mode, clk/8 = 12800Hz
-    OCR0 = 71;
+    // 50 Hz with 8bit resolution => update every 20ms and 256 samples => update every 0.15 ms
+    TCCR0 = ( 1 << WGM01 ) | ( 1 << CS01 );                     // Timer0 in CTC mode, clk/8 = 6444.8Hz
+    OCR0 = 142;
 
     // System timer
     TCCR2 = ( 1 << WGM21 ) | ( 1 << CS21 ) | ( 1 << CS20);     // Timer2 in CTC mode, clk/32
@@ -364,42 +399,79 @@ void setup(void){
     // Disable analog comparator
     ACSR = (1 << ACD);
 
+    compare_PWM0 = 0x80;
+    compbuff_PWM0 = 0x80;
+    compare_PWM1 = 0x30;
+    compbuff_PWM1 = 0x30;
+    compare_PWM2 = 0xAA;
+    compbuff_PWM2 = 0xAA;
+
     lcd_init();
     lcd_text("TEST");
-    lcd_move_cursor(1,0);
-    lcd_text("dupa 1");
-    lcd_move_cursor(2,1);
-    lcd_text("dupa 2");
-    lcd_move_cursor(3,2);
-    lcd_text("dupa 3");
+
     sei();
 }
 
 int main(void){
     setup();
-    uint8_t hex[16] = "0123456789ABCDEF";
+    unsigned char hex[16] = "0123456789ABCDEF";
     for(;;){
         wait_ms(100);
-        PORTA ^= ( 1 << PA7 );
 
         lcd_move_cursor(3, 10);
-        lcd_data((hour >> 4) + 0x30);
-        lcd_data((hour & 0x0F) + 0x30);
-        lcd_data(0x3A);
-        lcd_data((min >> 4) + 0x30);
-        lcd_data((min & 0x0F) + 0x30);
-        lcd_data(0x3A);
-        lcd_data((sec >> 4) + 0x30);
+        lcd_data((hour >> 4) + '0');
+        lcd_data((hour & 0x0F) + '0');
+        lcd_data(':');
+        lcd_data((min >> 4) + '0');
+        lcd_data((min & 0x0F) + '0');
+        lcd_data(':');
+        lcd_data((sec >> 4) + '0');
         lcd_move_cursor(3, 17);
         lcd_text(" ");
         lcd_move_cursor(3, 17);
-        lcd_data((sec & 0x0F) + 0x30);
+        lcd_data((sec & 0x0F) + '0');
 
         lcd_move_cursor(0, 10);
+        lcd_data(actual_num_key);
+        lcd_data(actual_func_key);
+
+        switch (actual_num_key){
+            case '0':
+                compbuff_PWM0 = 0;
+            break;
+            case '1':
+                compbuff_PWM0 = 3;
+            break;
+            case '2':
+                compbuff_PWM0 = 9;
+            break;
+            case '3':
+                compbuff_PWM0 = 12;
+            break;
+            case '4':
+                compbuff_PWM0 = 15;
+            break;
+            case '5':
+                compbuff_PWM0 = 18;
+            break;
+            case '6':
+                compbuff_PWM0 = 21;
+            break;
+            case '7':
+                compbuff_PWM0 = 24;
+            break;
+            case '8':
+                compbuff_PWM0 = 27;
+            break;
+            case '9':
+                compbuff_PWM0 = 30;
+            break;
+        }
+        /*lcd_move_cursor(0, 10);
         lcd_data(hex[key_col_state[0]]);
         lcd_data(hex[key_col_state[1]]);
         lcd_data(hex[key_col_state[2]]);
-        lcd_data(hex[key_col_state[3]]);
+        lcd_data(hex[key_col_state[3]]);*/
         //USART_text((unsigned char)"test UART\n\r");
     }
 }
